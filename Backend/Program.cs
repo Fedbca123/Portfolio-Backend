d7cdf7205a95594f925ddc404280de391213202a
Omar Shalaby <omarshalaby911@knight.ucf.edu>
2024-01-15 22:10:13 -0500

    pulls Readme.md from repos and converts to JSON

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,46 +1,27 @@
-using System;
+using System.Dynamic;
 using System.Threading.Tasks;
 using GraphQL;
 using GraphQL.Client.Http;
 using GraphQL.Client.Serializer.Newtonsoft;
 using dotenv.net;
+using Markdig;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 public class Program
 {
     public static async Task Main(string[] args)
+    {
+        await GetGithubInfo();
+    }
+
+    public static async Task GetGithubInfo()
     {
         DotEnv.Load();
         var token = Environment.GetEnvironmentVariable("GITHUB_ACCESS_TOKEN");
 
         var graphQLClient = new GraphQLHttpClient("https://api.github.com/graphql", new NewtonsoftJsonSerializer());
         graphQLClient.HttpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
-
-        var query = new GraphQLRequest
-        {
-            Query = @"
-            {
-                viewer {
-                    pinnedItems(first: 6, types: REPOSITORY) {
-                        edges {
-                            node {
-                                ... on Repository {
-                                    name
-                                    description
-                                    url
-                                    languages(first: 10) {
-                                        edges {
-                                            node {
-                                                name
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }"
-        };
 
         // var query = new GraphQLRequest
         // {
@@ -52,29 +33,8 @@ public class Program
         //                     node {
         //                         ... on Repository {
         //                             name
+        //                             description
         //                             url
-        //                             defaultBranchRef {
-        //                                 target {
-        //                                     ... on Commit {
-        //                                         history(first: 1) {
-        //                                             edges {
-        //                                                 node {
-        //                                                     tree {
-        //                                                         entries {
-        //                                                             name
-        //                                                             object {
-        //                                                                 ... on Blob {
-        //                                                                     text
-        //                                                                 }
-        //                                                             }
-        //                                                         }
-        //                                                     }
-        //                                                 }
-        //                                             }
-        //                                         }
-        //                                     }
-        //                                 }
-        //                             }
         //                             languages(first: 10) {
         //                                 edges {
         //                                     node {
@@ -90,8 +50,78 @@ public class Program
         //     }"
         // };
 
+        // var response = await graphQLClient.SendQueryAsync<dynamic>(query);
+
+        // // string markdown = /* your markdown content */;
+        // // string plainText = Markdig.Markdown.ToPlainText(markdown);
+
+        // // Console.WriteLine(response.Data);
+
+        // string json = JsonConvert.SerializeObject(response.Data, Formatting.Indented);
+
+        // Console.WriteLine(json);
+
+        // Modify your GraphQL query to fetch the README file
+        var query = new GraphQLRequest
+        {
+            Query = @"
+            {
+                viewer {
+                    pinnedItems(first: 6, types: REPOSITORY) {
+                        edges {
+                            node {
+                                ... on Repository {
+                                    name
+                                    url
+                                    languages(first: 10) {
+                                        edges {
+                                            node {
+                                                name
+                                            }
+                                        }
+                                    }
+                                    object(expression: ""main:README.md"") {
+                                        ... on Blob {
+                                            text
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }"
+        };
+
         var response = await graphQLClient.SendQueryAsync<dynamic>(query);
 
-        Console.WriteLine(response.Data);
+        var projects = new List<ExpandoObject>();
+
+        foreach (var node in (IEnumerable<dynamic>)response.Data.viewer.pinnedItems.edges)
+        {
+            dynamic project = new ExpandoObject();
+            project.name = node.node.name;
+            project.url = node.node.url;
+            project.stack = ((IEnumerable<dynamic>)node.node.languages.edges).Select(lang => (string)lang.node.name).ToArray();
+
+            // Add a null check before accessing the text property
+            if (node.node.@object != null)
+            {
+                project.description = Markdown.ToPlainText(node.node.@object.text.ToString());
+            }
+            else
+            {
+                project.description = null;
+            }
+
+            dynamic projectWrapper = new ExpandoObject();
+            projectWrapper.project = project;
+
+            projects.Add(projectWrapper);
+        }
+
+        string json = JsonConvert.SerializeObject(projects, Formatting.Indented);
+
+        Console.WriteLine(json);
     }
 }
